2a104d1c78525548e75695e8618d0692654dc4ef
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-05-13 21:03:03 -0500

    feat: :tada: Initial Project

---
+++ src/main/java/proyecto_spring_boot_java/proyecto_spring_boot_java/ProyectoSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package proyecto_spring_boot_java.proyecto_spring_boot_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto_spring_boot_java/proyecto_spring_boot_java/ProyectoSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_spring_boot_java.proyecto_spring_boot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
