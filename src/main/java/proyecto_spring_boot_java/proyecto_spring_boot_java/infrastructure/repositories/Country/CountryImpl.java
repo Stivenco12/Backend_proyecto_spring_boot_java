6495108d3750cb02d4b9ca4b152bf9ce990984d4
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-05-15 16:03:24 -0500

    feat: ✨implement Address and Country repository and service classes

---
+++ src/main/java/proyecto_spring_boot_java/proyecto_spring_boot_java/infrastructure/repositories/Country/CountryImpl.java
@@ -0,0 +1,54 @@
+package proyecto_spring_boot_java.proyecto_spring_boot_java.infrastructure.repositories.Country;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import proyecto_spring_boot_java.proyecto_spring_boot_java.Domain.entities.Country;
+import proyecto_spring_boot_java.proyecto_spring_boot_java.application.services.ICountryService;
+
+@Service
+public class CountryImpl implements ICountryService {
+    @Autowired
+    private ICountryService repository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Country> findAll() {
+        return (List<Country>) repository.findAll();
+    }
+
+    @Override
+    public Optional<Country> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Country save(Country country) {
+        return repository.save(country);
+    }
+
+    @Override
+    public Optional<Country> update(Long id, Country country) {
+        Optional<Country> countryOld = repository.findById(id);
+        if(countryOld.isPresent()){
+            Country countryDb = countryOld.orElseThrow();
+            countryDb.setName(country.getName());
+            return Optional.of(repository.save(countryDb));
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Country> delete(Long id) {
+        Optional<Country> countryOptional = repository.findById(id);
+        countryOptional.ifPresent(countrytDb -> {
+            repository.delete(countrytDb.getId());
+        });
+        return countryOptional;        
+    }
+
+
+
+}
