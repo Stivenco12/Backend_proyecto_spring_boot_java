bf86f4ab27ef6715c81142c6bb1f897d4ecf40d5
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-05-18 13:57:17 -0500

    feat: ✨ add CRUD operations for Reservations, Returns, Suppliers, and Tools with validation in respective controllers

---
+++ src/main/java/proyecto_spring_boot_java/proyecto_spring_boot_java/infrastructure/controllers/ReservationsController.java
@@ -0,0 +1,71 @@
+package proyecto_spring_boot_java.proyecto_spring_boot_java.infrastructure.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+import proyecto_spring_boot_java.proyecto_spring_boot_java.Domain.entities.Reservations;
+import proyecto_spring_boot_java.proyecto_spring_boot_java.application.services.IReservationsService;
+
+@RestController
+@RequestMapping("/api/Reservations")
+public class ReservationsController {
+
+    @Autowired
+    private IReservationsService reservationsService;
+
+    @GetMapping
+    public List<Reservations> list() {
+        return reservationsService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> view(@PathVariable Long id) {
+        Optional<Reservations> optional = reservationsService.findById(id);
+        if (optional.isPresent()) {
+            return ResponseEntity.ok(optional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody Reservations reservation, BindingResult result) {
+        if (result.hasErrors()) {
+            return validation(result);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(reservationsService.save(reservation));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody Reservations reservation, @PathVariable Long id) {
+        Optional<Reservations> optional = reservationsService.update(id, reservation);
+        if (optional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(optional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<Reservations> optional = reservationsService.delete(id);
+        if (optional.isPresent()) {
+            return ResponseEntity.ok(optional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, Object> response = new HashMap<>();
+        result.getFieldErrors().forEach(err -> {
+            String message = "Error in field '" + err.getField() + "': " + err.getDefaultMessage();
+            response.put("error", message);
+        });
+        return ResponseEntity.badRequest().body(response);
+    }
+}
