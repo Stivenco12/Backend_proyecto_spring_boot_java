c694eb4550494ec105632bea5bb3a9545479ddf2
osman019 <osmanortizrolon@gmail.com>
2025-05-22 16:04:24 -0500

    feat: ✨ update ToolRequestDTO and Tools entities; add image handling and modify database connection properties

--- src/main/java/proyecto_spring_boot_java/proyecto_spring_boot_java/Domain/entities/Tools.java
+++ src/main/java/proyecto_spring_boot_java/proyecto_spring_boot_java/Domain/entities/Tools.java
@@ -21,6 +21,10 @@ public class Tools {
     @Column(name = "datos_imagen", nullable = false)
     private byte[] datosImagen;
 
+    public String getImagenBase64() {
+    return datosImagen != null ? java.util.Base64.getEncoder().encodeToString(datosImagen) : null;
+}
+
     @Column(length = 50)
     private String brand;
 
